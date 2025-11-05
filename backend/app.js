9fa43535db13f7f0e4f8d1a75da62c656fc93103
hey-Sam-u <samirpatil1626@gmail.com>
2025-11-05 10:20:16 +0530

    fix express v5 path to regexp issue

--- backend/app.js
+++ backend/app.js
@@ -774,7 +774,7 @@ app.post("/api/delete-account", verifyToken, (req, res) => {
 });
 const frontendPath =path.join(__dirname, "../frontend");
 app.use(express.static(frontendPath));
-app.get("*",(req, res)=>{
+app.get("/*",(req, res)=>{
   res.sendFile(path.join(frontendPath,"index.html"));
 });
 
